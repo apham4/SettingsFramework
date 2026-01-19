55e3bcc2ec94e9ef15d7e5245b0e161f8a52ae65
Anh Pham <phamquocanh2580@gmail.com>
2026-01-19 16:54:31 -0500

    Add Win64 as allowed platform + add SFSettingsSubsystem files

---
+++ Source/SettingsFramework/Public/SFSettingsSubsystem.h
@@ -0,0 +1,17 @@
+// Copyright 2026 Anh Pham. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "SFSettingsSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SETTINGSFRAMEWORK_API USFSettingsSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
