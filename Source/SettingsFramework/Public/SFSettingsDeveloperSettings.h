37bf765491eb769332061689b0b4872645af3f25
Anh Pham <phamquocanh2580@gmail.com>
2026-04-03 20:37:31 -0400

    Add comments for dev settings & helper classes

--- Source/SettingsFramework/Public/SFSettingsDeveloperSettings.h
+++ Source/SettingsFramework/Public/SFSettingsDeveloperSettings.h
@@ -8,51 +8,85 @@
 #include "SFSettingsDeveloperSettings.generated.h"
 
 /**
- * Developer settings for the SettingsFramework plugin. TODO: Add all these in instructions documentation.
+ * @brief Developer settings to define project-wide configurations for the SettingsFramework plugin.
+ *
+ * Developer settings to define project-wide configurations for the SettingsFramework plugin. 
+ * Accessed through **Project Settings** > **Plugins** > **SettingsFramework** in the Unreal Editor.
+ * @note @configgame
  */
-UCLASS(Config = Game, DefaultConfig, meta = (DisplayName = "Settings Framework"))
+UCLASS(Config = Game, meta = (DisplayName = "Settings Framework", ToolTip = "Developer settings to define project-wide configurations for the SettingsFramework plugin."))
 class SETTINGSFRAMEWORK_API USFSettingsDeveloperSettings : public UDeveloperSettings
 {
 	GENERATED_BODY()
 	
 public:
-	// The master registry asset containing setting hierarchy and definitions. Loaded asynchronously on SFSettingsSubsystem initialization.
+	/**
+	* The master registry asset containing setting hierarchy and definitions. Loaded asynchronously on USFSettingsSubsystem initialization.
+	* @note @config @ea
+	*/
 	UPROPERTY(Config, EditAnywhere, Category = "Settings Registry")
 	TSoftObjectPtr<class USFSettingsRegistry> SettingsRegistry;
 
-	// Save slot name for saving and loading setting values.
+	/**
+	* Save slot name for saving and loading setting values to and from disk.
+	* @note @config @ea
+	*/
 	UPROPERTY(Config, EditAnywhere, Category = "Settings Serialization")
 	FString SaveGameSlotName = TEXT("SF_SaveSlot");
 
-	// Recursion guard: Maximum depth for updating setting dependencies to prevent infinite loops.
+	/**
+	* The recursion guard for conditional dependencies: Maximum depth for updating setting dependencies to prevent infinite loops. If the depth is exceeded, a warning will be logged and the update will be halted.
+	* @note @config @ea
+	*/
 	UPROPERTY(Config, EditAnywhere, Category = "Settings Conditions")
 	int32 MaxUpdateDepth = 10;
 
-	// Default keybind collision resolution policy when adding a new keybind that conflicts with an existing one.
+	/**
+	* The default keybind collision resolution policy when adding a new keybind that conflicts with an existing one.
+	* @note @config @ea
+	*/
 	UPROPERTY(Config, EditAnywhere, Category = "Settings Logic")
 	ESFKeybindCollisionResolution DefaultKeybindCollisionResolution = ESFKeybindCollisionResolution::Overwrite;
 
-	// Widget class for settings UI's root category tab buttons.
+	/**
+	* The widget class for the Settings Screen UI's root category tab buttons.
+	* @note @config @ea
+	*/
 	UPROPERTY(Config, EditAnywhere, Category = "Settings UI")
 	TSubclassOf<class USFCategoryTabButtonBase> RootTabButtonClass;
 
-	// Widget class for settings UI's branch category tab buttons.
+	/**
+	* The widget class for the Settings Screen UI's branch category tab buttons.
+	* @note @config @ea
+	*/
 	UPROPERTY(Config, EditAnywhere, Category = "Settings UI")
 	TSubclassOf<class USFCategoryTabButtonBase> BranchTabButtonClass;
 
-	// Widget class for displaying a branch-type category (contains sub-categories).
+	/**
+	* The widget class for displaying the content of a Branch category (containing sub-categories) in the Settings Screen UI.
+	* @note @config @ea
+	*/
 	UPROPERTY(Config, EditAnywhere, Category = "Settings UI")
 	TSubclassOf<class USFCategoryTab_Branch> BranchTabContentClass;
 
-	// Widget class for displaying a leaf-type category (contains setting entries).
+	/**
+	* The widget class for displaying the content of a Leaf category (containing setting groups and setting entries) in the Settings Screen UI.
+	* @note @config @ea
+	*/
 	UPROPERTY(Config, EditAnywhere, Category = "Settings UI")
 	TSubclassOf<class USFCategoryTab_Leaf> LeafTabContentClass;
 
-	// Widget class for displaying a setting group (visually groups related settings within a leaf category).
+	/**
+	* The widget class for displaying a setting group in the Settings Screen UI.
+	* @note @config @ea 
+	*/
 	UPROPERTY(Config, EditAnywhere, Category = "Settings UI")
 	TSubclassOf<class USFSettingGroupWidget> SettingGroupWidgetClass;
 
-	// Mapping of Definition Classes to Widget Classes (e.g. USFSettingDefinition_Scalar -> WBP_Setting_Slider)
+	/**
+	* The mapping of widget classes to display each variation of setting definition (e.g. a slider widget for a scalar setting) in the Settings Screen UI.
+	* * @note @config @ea
+	*/
 	UPROPERTY(Config, EditAnywhere, Category = "Settings UI")
 	TMap<TSubclassOf<class USFSettingDefinition>, TSubclassOf<class USFSettingEntryWidget>> SettingEntryWidgetClasses;
 
